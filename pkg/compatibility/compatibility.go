fef4cff6892eb3b3f2ccd9fcf35b0bb3f49c1a66
vol <me@volodymyrstoiko.com>
2023-12-17 23:26:08 +0200

    podnames

--- pkg/compatibility/compatibility.go
+++ pkg/compatibility/compatibility.go
@@ -1,7 +1,12 @@
 package compatibility
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -29,11 +34,32 @@ func (c *Compatibility) Run() error {
 
 	dsName := "kubeshark-kernel-version"
 	namespace := "default"
+	timeout := 2 * time.Minute
 
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	c.logger.Infof("creating %s/%s daemonset", namespace, dsName)
 	_, err = c.createUnameDaemonSet(dsName, namespace)
 	if err != nil {
 		return err
 	}
+	c.logger.Infof("daemonset %s/%s created", namespace, dsName)
+
+	podNames, err := c.waitForDaemonSetPodsRunning(ctx, dsName, namespace)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(podNames)
+
+	c.logger.Infof("cleaning up %s/%s daemonset", namespace, dsName)
+	deleteOptions := metav1.DeleteOptions{}
+	err = c.clientset.AppsV1().DaemonSets(namespace).Delete(context.TODO(), dsName, deleteOptions)
+	if err != nil {
+		return err
+	}
+	c.logger.Infof("daemonset %s/%s cleaned up", namespace, dsName)
 
 	return nil
 }
--- pkg/compatibility/daemonset.go
+++ pkg/compatibility/daemonset.go
@@ -3,10 +3,12 @@ package compatibility
 import (
 	"context"
 	"fmt"
+	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 func (c *Compatibility) createUnameDaemonSet(dsName, namespace string) (*appsv1.DaemonSet, error) {
@@ -46,4 +48,36 @@ func (c *Compatibility) createUnameDaemonSet(dsName, namespace string) (*appsv1.
 	}
 
 	return ds, nil
+}
+
+func (c *Compatibility) waitForDaemonSetPodsRunning(ctx context.Context, dsName, namespace string) ([]string, error) {
+	var podNames []string
+	timeout := 5 * time.Second
+	immediate := true
+
+	err := wait.PollUntilContextCancel(ctx, timeout, immediate, func(ctx context.Context) (bool, error) {
+		pods, err := c.clientset.CoreV1().Pods("").List(ctx, metav1.ListOptions{
+			LabelSelector: "app=" + dsName,
+		})
+		if err != nil {
+			return false, err
+		}
+
+		podNames = make([]string, 0)
+		allRunning := true
+		for _, pod := range pods.Items {
+			if pod.Status.Phase != corev1.PodRunning {
+				allRunning = false
+				break
+			}
+			podNames = append(podNames, pod.Name)
+		}
+
+		return allRunning, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return podNames, nil
 }
