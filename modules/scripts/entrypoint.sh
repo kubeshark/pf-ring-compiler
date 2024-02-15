cfe060456c8f8bb74a847cd84e475d64404e7cd3
vol <me@volodymyrstoiko.com>
2024-02-15 14:47:20 +0200

    Add modules

--- modules/scripts/entrypoint.sh
+++ modules/scripts/entrypoint.sh
@@ -10,7 +10,7 @@ if [ -f "$module_path" ]; then
     # Check if the module is already loaded
     if ! lsmod | grep -q pf_ring; then
         echo "Loading pf_ring module for kernel ${kernel_version}"
-        module_load_path="/opt/modules/pf-ring-${kernel_version}.ko"
+        module_load_path="/opt/lib/modules/pf-ring-${kernel_version}.ko"
         mkdir /opt/lib/modules/${kernel_version} -p
         cp $module_path ${module_load_path}
         depmod -b /opt ${kernel_version}
